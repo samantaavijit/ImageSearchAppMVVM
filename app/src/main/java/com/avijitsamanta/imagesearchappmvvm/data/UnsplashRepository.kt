6a154dbb2eec04bf42740a2e667a111a5330f97d
Avijit Samanta <samantaavijit.2000@gmail.com>
2020-11-29 18:20:23 +0530

    "Add Pagination"

--- app/src/main/java/com/avijitsamanta/imagesearchappmvvm/data/UnsplashRepository.kt
+++ app/src/main/java/com/avijitsamanta/imagesearchappmvvm/data/UnsplashRepository.kt
@@ -1,9 +1,23 @@
 package com.avijitsamanta.imagesearchappmvvm.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.avijitsamanta.imagesearchappmvvm.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                UnsplashPagingResource(unsplashApi, query)
+            }
+        ).liveData
 }
